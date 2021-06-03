25fed213833085160ba9c89da8f8b272b22209a5
mikeangelsilva <38802880+mikeangelsilva@users.noreply.github.com>
2021-06-03 17:28:55 +0000

    Deploying to gh-pages from  @ 67675ae54b02b8b6283f6148466c9df341e4e00e 🚀

--- _next/static/chunks/pages/list-8ef855bc06b8164588f0.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[43],{hKUc:function(n,c,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/list",function(){return i("oVyb")}])},oVyb:function(n,c,i){"use strict";i.r(c);var t=i("nKUr");i("q1tI");c.default=function(){return Object(t.jsx)("div",{children:Object(t.jsx)("h1",{children:"dsada"})})}}},[["hKUc",0,2,1]]]);
