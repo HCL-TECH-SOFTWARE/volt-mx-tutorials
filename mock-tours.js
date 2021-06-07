73f13b843c1d6d2f1e897f5a5968c9dcdecb01d7
Michael Silva <michangelosilva@gmail.com>
2021-06-08 02:00:20 +0800

    mock tours.json

--- mock-tours.js
+++ mock-tours.js
@@ -4,40 +4,46 @@ const { getHikeDirectories, HIKES_CONTENT_PATH } = require("./hike.config");
 const MOCK_PATH = "./public/hike";
 const hikeDirectories = getHikeDirectories();
 
-hikeDirectories.forEach((directory) => {
-  fs.readFile(`${HIKES_CONTENT_PATH}/${directory}/tours.json`, "utf8", function(
-    err,
-    data
-  ) {
-    if (err) throw err;
+// Creates /public/hike/tour/details
+fs.mkdir(`${MOCK_PATH}/tour/details`, { recursive: true }, (err) => {
+  // mock response of asset_hike_map
+  const hike_map = {
+    details: [[]],
+    status: 200,
+  };
 
-    const tours = JSON.parse(data).categoryTours;
+  fs.writeFile(
+    `${MOCK_PATH}/asset_hike_map.json`,
+    JSON.stringify(hike_map, null, 2),
+    "utf-8",
+    () => {
+      console.log("mocked asset_hike_map endpoint");
+    }
+  );
 
-    tours.forEach((tour) => {
-      // mock response of each hike/tour details
-      fs.writeFile(
-        `${MOCK_PATH}/tour/details/${tour.kuid}.json`,
-        JSON.stringify(tour, null, 2),
-        "utf-8",
-        () => {
-          console.log(`copied: ${tour.kuid}`);
-        }
-      );
-    });
+  hikeDirectories.forEach((directory) => {
+    fs.readFile(
+      `${HIKES_CONTENT_PATH}/${directory}/tours.json`,
+      "utf8",
+      function(err, data) {
+        if (err) throw err;
+
+        const tours = JSON.parse(data).categoryTours;
+
+        tours.forEach((tour) => {
+          // mock response of each hike/tour details
+          fs.writeFile(
+            `${MOCK_PATH}/tour/details/${tour.kuid}.json`,
+            JSON.stringify(tour, null, 2),
+            "utf-8",
+            () => {
+              console.log(`copied: ${tour.kuid}.json`);
+            }
+          );
+        });
+      }
+    );
   });
+
+  if (err) throw err;
 });
-
-// mock response of asset_hike_map (200 - success)
-const hike_map = {
-  details: [[]],
-  status: 200,
-};
-
-fs.writeFile(
-  `${MOCK_PATH}/asset_hike_map.json`,
-  JSON.stringify(hike_map, null, 2),
-  "utf-8",
-  () => {
-    console.log("mocked asset_hike_map endpoint");
-  }
-);
