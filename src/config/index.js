4924b9f9872cba7264c3d928d66677842030c603
Hon Lam So <lam10301995@gmail.com>
2022-05-03 18:49:42 +0800

    configs update for gh-pages

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,9 @@ export const isDev = process.env.NODE_ENV !== 'production';
 export const DEV_PORT = 3200;
 export const BASE_BRANCH = 'master';
 
-export const HOST_URL = `https://opensource.hcltechsw.com`;
+export const HOST_URL = `https://honlamso.github.io/volt-mx-tutorials`;
 export const BASE_PATH_URL = 'volt-mx-tutorials';
 
-export const GITHUB_NAME = 'HCL-TECH-SOFTWARE';
+export const GITHUB_NAME = 'honlamso';
 
 export const SERVER = isDev ? `http://localhost:${DEV_PORT}` : `${HOST_URL}/${BASE_PATH_URL}`;
