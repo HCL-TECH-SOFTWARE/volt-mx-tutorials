563880a50a2260c6c234a3ae296fe57528bd5018
Hon Lam So <lam10301995@gmail.com>
2022-05-03 19:00:59 +0800

    fix url

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export const isDev = process.env.NODE_ENV !== 'production';
 export const DEV_PORT = 3200;
 export const BASE_BRANCH = 'master';
 
-export const HOST_URL = `https://honlamso.github.io/volt-mx-tutorials`;
+export const HOST_URL = `https://honlamso.github.io`;
 export const BASE_PATH_URL = 'volt-mx-tutorials';
 
 export const GITHUB_NAME = 'honlamso';
