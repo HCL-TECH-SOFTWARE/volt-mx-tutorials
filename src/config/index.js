18de804dd17b6465bec18567ae5ada79d9a64700
rusty.godwin@hcl.com <rusty.godwin@hcl.com>
2025-02-06 13:11:06 -0500

    Kick off action

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const isDev = process.env.NODE_ENV !== 'production';
 export const DEV_PORT = 3200;
-export const BASE_BRANCH = 'voltformula-hikes';
+export const BASE_BRANCH = 'voltformula-hikes'; // phx-dev-9.5
 
 export const HOST_URL = `https://rusty-godwin-hcl.github.io`;
 export const BASE_PATH_URL = 'volt-mx-tutorials';
